9f6045ea955060c743eaefdf8a279f26b6a3f8bb
Jitesh Varade <143549511+jiteshvarade@users.noreply.github.com>
2024-06-03 01:23:42 +0530

    Update auth.mjs

--- server/src/middlewares/auth.mjs
+++ server/src/middlewares/auth.mjs
@@ -2,24 +2,30 @@ import Router from "express"
 const router = Router()
 import User from "../models/user.mjs"
 
-router.post("/",async (req,res)=>{
-    const data = req.body
+router.post("/", async (req, res) => {
+  const data = req.body
 
-    try {
-
-        if (!data) {
-            return res.status(400).json({ message: 'Please send required user data' })
-        }
-
-        const UserInfo = new User({UserInfo : data})
-        await UserInfo.save()
-
-        console.log("User created successfully!")
-        res.status(201).json({ message: 'User created successfully!', data: UserInfo })
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ message: 'Server error: could not could not cerate user' })
+  try {
+    if (!data) {
+      return res.status(400).json({ message: "Please send required user data" })
     }
+
+    let email = data.email
+    const existingUser = await User.findOne({ "UserInfo.email" : email})
+
+    if (existingUser) {
+      console.log("Email already exists in the database.")
+      return res.status(409).json({ message: "Email already exists" })
+    } else {
+      const newUser = new User({UserInfo : data})
+      await newUser.save()
+      console.log("User created successfully!")
+      res.status(201).json({ message: "User created successfully!", data: newUser })
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: "Server error: could not create user" })
+  }
 })
 
 export default router
