c8c13423826c8575855d6ab51dbeac65049da5f3
Aakash Kaushik <aakshkaushik2@gmail.com>
2024-06-10 13:20:33 +0530

    Img and skills functnality added

--- client/src/Components/ProjectAndEProfile/ProfileEdit.jsx
+++ client/src/Components/ProjectAndEProfile/ProfileEdit.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaLongArrowAltLeft } from "react-icons/fa";
-
+import { MdModeEditOutline } from "react-icons/md";
 import './project.css'
 import GradientBtn from '../Buttons/GradientBtn';
 
-function ProfileEdit() {
+
+function ProfileEdit(e) {
+    const skills = [];
+    function skillpush () {
+        document.querySelectorAll('.skills').forEach(e => skills.push(e.value))
+        console.log(skills)
+    }
+
+    const [image, setImage] = useState('https://cdn.pixabay.com/photo/2016/11/22/21/26/asian-girl-1850617_1280.jpg');
+    function handelImageUpload (e) {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImage(reader.result);
+        };
+        if(file){
+            reader.readAsDataURL(file);
+        }
+    }
+    const [proImg, setProImg] = useState('"https://i.pinimg.com/236x/fe/af/94/feaf944be5749d956396ebc4662ca39e.jpg"')
     return (
         <>
             <div className='border-2 bg-[#0f1325]  border-red-400   w-[100%] text-white'>
@@ -42,35 +62,64 @@ function ProfileEdit() {
                                     />
                                 </div>
                             </div>
-                            <div className='w-[100%] md:w-[20%] mb-4 ' >
+                            <div className='mx-auto w-fit md:w-[20%] md:mx-0 mb-4 relative' >
                                 <div >
-                                    <img src="https://i.pinimg.com/236x/fe/af/94/feaf944be5749d956396ebc4662ca39e.jpg" className='max-h-[200px] rounded-lg mx-auto' alt="" />
-                                    {/* <input type="file" name="" id="" /> */}
+                                    <img src={image} className='max-h-[200px] rounded-lg mx-auto' alt="" />
+                                    <div className='absolute bottom-[30px]  right-[-10%] lg:right-[15%] ' >
+                                        <div className='flex w-[50px] '>
+                                            <div className=''>
+                                            <div className='bg-white absolute top-0 w-[30px] h-[30px] flex  justify-center items-center  rounded-full'>
+                                                    <span className='text-black text-2xl'>
+                                                        {/* {"sourceCodeFile" || 'Upload Source Code'} */}
+                                                        <MdModeEditOutline />
+                                                    </span>
+                                                </div>
+                                                <input
+                                                    type='file'
+                                                    accept='image/*'
+                                                    onChange={handelImageUpload}
+
+                                                    // ref={fileref}
+                                                    className='absolute  inset-0 border-none outline-none rounded-full  w-[30px] h-[30px] opacity-0 cursor-pointer'
+
+                                                />
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
                         </div>
                         <div className="w-full px-4 rounded-xl bg-[#0CA3E7] bg-opacity-[10%] py-2 mb-2">
-                                    <p className='font-semibold text-xl'>Bio</p>
-                                    <textarea name="" className='w-full resize-none rounded-full px-6 py-1 border-none outline-none bg-[#062c4a] shadow-[inset_0_0_5px_white]' id=""></textarea>
-                                </div>
+                            <p className='font-semibold text-xl'>Bio</p>
+                            <textarea name="" className='w-full resize-none rounded-full px-6 py-1 border-none outline-none bg-[#062c4a] shadow-[inset_0_0_5px_white]' id=""></textarea>
+                        </div>
                         <div>
-                        <div className="w-full px-4 rounded-xl bg-[#0CA3E7] bg-opacity-[7%] py-2 mb-2">
+                            <div className="w-full px-4 rounded-xl bg-[#0CA3E7] bg-opacity-[7%] py-2 mb-2">
                                 <p className='font-semibold text-xl'>Skills</p>
-                                <div className='py-2 flex flex-wrap gap-2'>
-                                    <input placeholder='Html' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' />
+                                <div id='skill' className='py-2 flex flex-wrap gap-2'>
+                                    {/* <input placeholder='Html' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' />
                                     <input placeholder='Css' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' />
                                     <input placeholder='JavaScript' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' />
-                                    <input placeholder='Phython' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' />
-                                    <span className=' px-3 rounded-xl bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white] font-bold text-xl'>+</span>
+                                    <input placeholder='Phython' type="text" className='w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]' /> */}
+                                    <span 
+                                    onClick={()=>{
+                                        const inputText = document.createElement('input')
+                                        inputText.setAttribute('type', 'text')
+                                        inputText.setAttribute('Class', 'skills w-[120px] px-4 rounded-xl border-none outline-none bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white]')
+                                        document.querySelector('#skill').appendChild(inputText)
+                                    }}
+                                    className='px-3 rounded-xl cursor-pointer bg-[#0CA3E7] bg-opacity-[7%] py-1 shadow-[inset_0_0_5px_white] font-bold text-xl'
+                                    >+</span>
                                 </div>
                             </div>
 
                         </div>
                         <div className='flex gap-4 mt-4'>
 
-                        <GradientBtn placeholder="Reset" />
-                        <GradientBtn placeholder="Save" />
+                            <GradientBtn placeholder="Reset" />
+                            <GradientBtn placeholder="Save" />
                         </div>
 
                     </div>
