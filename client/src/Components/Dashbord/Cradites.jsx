fbfd89cdc9a94e00d50b5478054d3608b57f20c4
AnkitChauhan39 <ankitchauhanbly88@gmail.com>
2024-06-10 17:21:46 +0530

    connetion establish

--- client/src/Components/Dashbord/Cradites.jsx
+++ client/src/Components/Dashbord/Cradites.jsx
@@ -1,12 +1,97 @@
 import React from 'react'
 import craditSecImg from '../../../public/Icons/craditesSectionImg.png'
 import craditeSecBg from '../../../public/Icons/craditeBG.png'
+import GradientBtn from '../Buttons/GradientBtn'
+import RazorpayButton from '../RazorpayButton/razorpaybutton'
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from 'react'
+import photo from "../../assets/AuctionroomImages/mdi_eye.png"
 
 
-function Cradites() {
+
+function Cradites({credits=0}) {
+
+    const [Amount,setAmount] = useState('') ; 
+    const { user } = useAuth0();
+    const [sig,setsig] = useState(null)
+
+    function loadRazorPay()
+      {
+        const script = document.createElement('script')
+        script.src = "https://checkout.razorpay.com/v1/checkout.js"
+        document.body.appendChild(script)
+        script.onload = handleSubmit
+      }
+
+      async function handleSubmit()
+  {
+    const amount = (Amount*100)
+    console.log(Amount)
+    try {
+      const response = await fetch("https://devauction.onrender.com/payments", {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({amount : amount}),
+      });
+  
+      const result = await response.json();
+    //   console.log("Success:", result);
+
+      if(response.ok)
+      {
+          var options = {
+              "key_id" : "rzp_test_w1PnHafmCNsrDy", 
+              "amount": `${result.amount}`, 
+              "currency": "INR",
+              "name": "DevAuction",
+              "description": "Test Transaction",
+              "image": {photo},
+              "order_id": `${result.id}`, 
+              "handler": function (response){  
+                  alert("Payment successfull!")
+              },
+              "prefill": {
+                  "name": `${user.name}`,
+                  "email": `${user.email}`,
+                  "contact": "9322679131"
+              },
+              "notes": {
+                  "address": "DevAuction Corporate Office"
+              },
+              "theme": {
+                  "color": "#3399cc"
+              }
+          };
+          console.log(options)
+
+          var rzp1 = new Razorpay(options);
+          await rzp1.open()
+          
+
+          rzp1.on('payment.failed', function (response){
+                  alert(response.error.code);
+                  alert(response.error.description);
+                  alert(response.error.source);
+                  alert(response.error.step);
+                  alert(response.error.reason);
+                  alert(response.error.metadata.order_id);
+                  alert(response.error.metadata.payment_id);
+          });
+      }
+
+    //   console.log(responsePayment)
+      
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+
     return (
         <>
-
             <section className='p-3' id="cradites">
                 <div className=' mx-auto lg:w-[100%] border-[#223534] bg-[#050b1e] border-2 flex items-center justify-between rounded-xl'>
                     <div style={{backgroundImage: 'url("../../../public/Icons/craditeBG.png")' }} className=' rounded-xl w-[100%] bg-cover bg-center'>
@@ -16,20 +101,20 @@ function Cradites() {
                             <div className='bg-[#0567FC] ml-5 flex rounded-xl text-lg bg-opacity-30 px-3 py-2'>
                                 ₹
                                 <input
+                                    value={Amount}
                                     type="number"
                                     placeholder='Amount'
+                                    onChange={ (e) => { 
+                                        setAmount(e.target.value)
+                                    }}
                                     className='border-none w-[100%] text-lg pl-1 outline-none bg-transparent text-white placeholder:text-white'
                                     name=""
                                     id="" />
                             </div>
-                            <button className='border-2 shadowButton ml-5 border-[#0CA3E7] text-[16px] flex justify-center items-center bg-gradient-to-t from-[#0CA3E7] px-3 to-[#050618] py-2 rounded-xl cursor-pointer'>
-                                Withdrow now
-                                <span class="material-symbols-outlined">
-                                    arrow_forward
-                                </span>
-                            </button>
+                            <GradientBtn placeholder="Withdraw" />
+                            <GradientBtn placeholder="Deposite" onClick={loadRazorPay} />
                         </div>
-                        <p className='text-[#0CA3E7] font-semibold mt-2 ml-5 mb-4'>panding (₹ 0)</p>
+                        <p className='text-[#0CA3E7] font-semibold mt-2 ml-5 mb-4'>Balance (₹ {credits})</p>
                     </div>
                     {/* <div className='h-[100%] hidden lg:block'>
                         <img src={craditSecImg} className='h-full object-cover rounded-xl' alt="" />
@@ -42,7 +127,7 @@ function Cradites() {
                         <h4 className='text-2xl font-semibold ml-3 my-3'>Transaction history</h4>
                         <div>
                             <div className='bg-[#0567FC] flex rounded-xl text-lg bg-opacity-30 px-3 py-2 w-[90%] mx-auto'>
-                                <span class="material-symbols-outlined">
+                                <span className="material-symbols-outlined">
                                     wallet
                                 </span>
                                 No Transaction history
@@ -54,7 +139,7 @@ function Cradites() {
                         <h4 className='text-2xl font-semibold ml-3 my-3'>Transaction history</h4>
                         <div>
                             <div className='bg-[#0567FC] flex rounded-xl text-lg bg-opacity-30 px-3 py-2 w-[90%] mx-auto'>
-                                <span class="material-symbols-outlined">
+                                <span className="material-symbols-outlined">
                                     wallet
                                 </span>
                                 No Transaction history
--- client/src/Components/Dashbord/Dashbord.jsx
+++ client/src/Components/Dashbord/Dashbord.jsx
@@ -3,14 +3,70 @@ import LeftNavbar from './LeftNavbar'
 import EarningCards from './EarningCards'
 import Header from './Header'
 import Cradites from './Cradites'
+import { useAuth0 } from "@auth0/auth0-react";
 import Auctionrooms from '../AuctionRoom/Auctionrooms'
 import Highestbidder from '../AuctionRoom/Highestbidder'
-import Createauction from '../AuctionRoom/Createauction'
+import Createauction from '../AuctionRoom/Createauction' 
 import Offers from './Offers'
 
 function Dashbord() {
     const [isnav, setisnav] = useState(false)
     const [show, setshow] = useState(false);
+    const { user } = useAuth0();
+    const [data,setdata] = useState(null) ; 
+    const [avg,setavg] = useState(0) ;
+    const [total,settotal] = useState(0) ; 
+    const [totalearn,settotalearn] = useState(0) ; 
+    const [credits,setcredits] = useState(0) ; 
+
+
+    // https://devauction.onrender.com/profile
+
+    const response = async () => {
+
+        const res = await fetch("https://devauction.onrender.com/profile", {
+            method: "POST",
+            body: JSON.stringify({email : user.email}),
+            headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+
+        const data =  await res.json() 
+        setdata(data) ; 
+        setcredits(data.userData.Profile.Credits)
+
+        let avgspend = 0 ; 
+        let totalspend = 0  ; 
+        const arr1 = data.userData.Profile.Spendings
+
+        if(arr1.length != 0 ) {
+            for(let i = 0 ; i < arr1.length ; i++ )
+            {
+                totalspend = totalspend + arr1[i].Amount ; 
+            }
+            avgspend = totalspend/arr1.length ; 
+            setavg(avgspend) ;
+            settotal(totalspend) ;
+        }
+
+        let totalEarn = 0 ; 
+        const arr2 = data.userData.Profile.Earnings  
+
+        if(arr2.length != 0 )
+        {
+            for(let i = 0 ; i < arr2.length ; i++ )
+            {
+                totalEarn = totalEarn + arr2[i].Amount ;
+            }
+            settotalearn(totalEarn) ;
+        }
+      }
+
+      response();
+
+      
+      
 
     return (
         <div className=' '>
@@ -31,12 +87,12 @@ function Dashbord() {
                     <Header isnav={isnav} setisnav={setisnav} />    
 
                     {/* earnings cards */}
-                    <EarningCards />
+                    <EarningCards earningAmount={totalearn} spandAmount={total} avgAmount={avg} />
 
                     {/* <Auction /> */}
                     <Auctionrooms show={show} setshow={setshow} />
                     <Highestbidder />
-                    <Cradites />
+                    <Cradites credits={credits} />
                 </div>
             </div>
 
