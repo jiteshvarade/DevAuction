a9b17dfe724ab0946c24a09c7c5e0ac6db6aff54
Nitish-u <70533456+Nitish-u@users.noreply.github.com>
2024-06-14 02:04:52 +0530

    live auction room cards (free and premium) and history added

--- client/src/Components/AuctionRoom/Auctionrooms.jsx
+++ client/src/Components/AuctionRoom/Auctionrooms.jsx
@@ -1,66 +1,172 @@
-import React, { useState } from 'react'
-import Card from './Card'
-import './Auctionroom.css'
-import 'primereact/resources/themes/saga-blue/theme.css';  
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import DotPaginator from './DotPaginator';
-import GradientBtn from '../Buttons/GradientBtn'
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import "./Auctionroom.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import DotPaginator from "./DotPaginator";
+import GradientBtn from "../Buttons/GradientBtn";
+import { IoIosArrowDown } from "react-icons/io";
+import { RiRefreshLine } from "react-icons/ri";
 
 const Auctionrooms = ({ show, setshow }) => {
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(3);
+  const [selectOption, setSelectOption] = useState("free");
+  const roomOptions = ["free", "premium", "history"];
+  const [showOptions, setShowOptions] = useState(false);
+  const [roomData, setRoomData] = useState({
+    freeRooms: [],
+    premiumRooms: [],
+    history: [],
+  });
 
-    const [first, setFirst] = useState(0);
-    const [rows, setRows] = useState(3);
+  // const cards = Array.from({ length: 25 }, (_, index) => <Card key={index} />);
 
-    const cards = Array.from({ length: 25 }, (_, index) => <Card key={index} />);
+  useEffect(async () => {
+    console.log("pulling all data");
+    try {
+      const res = await fetch(
+        "http://in1.localto.net:5947/dashboard/getRooms",
+        {
+          method: "POST",
+          body: JSON.stringify({ type: "all" }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const roomData = await res.json(); // wo yaha hoga
+      setRoomData(roomData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-    const onPageChange = (event) => {
-        setFirst(event.first);
-        setRows(event.rows); 
-    };
+  async function refresh(type) {
+    try {
+      const res = await fetch(
+        "http://in1.localto.net:5947/dashboard/getRooms",
+        {
+          method: "POST",
+          body: JSON.stringify({ type: type }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const roomData = await res.json();
+      setRoomData((prevState) => {
+        return {
+          ...prevState,
+          [Object.keys(roomData)[0]]: [
+            ...prevState[Object.keys(roomData)[0]],
+            ...roomData[Object.keys(roomData)[0]],
+          ],
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    return (
+  const onPageChange = (event) => {
+    setFirst(event.first);
+    setRows(event.rows);
+  };
 
-        <div className=' bg-[#050618] flex flex-col gap-4' >
-
-            <div className='p-6 flex flex-col gap-4'>
-
-                <div className='flex justify-between flex-wrap gap-8' >
-                    <div className=' text-[28px] md:text-[40px] font-semibold w-[300px] text-white'>Auction Room</div>
-                    <GradientBtn placeholder="Create Rooms" onClick={ () => {
-                        setshow(!show)
-                    }} />
-                </div>
-
-                <div className='text-white flex gap-4 text-[18px] md:text-[24px] mt-4 font-semibold'>
-                    <div className=' border-r-2 pr-3 ' >
-                        Free Rooms
-                    </div>
-                    <div className='opacity-60 pr-3 '>
-                        Premium Rooms
-                    </div>
-                </div>
-
-                <div className=' flex flex-col justify-center items-center w-[100%]   ' >
-                    
-                    <div className='flex flex-wrap gap-2 justify-evenly transition-all duration-200'>
-                        {cards.slice(first, first + rows).map((card) => card)}
-                    </div>
-                </div>
-
-            </div>
-
-            <DotPaginator
-                        className='bg-[#050618] custom-paginator'
-                        first={first}
-                        rows={rows}
-                        totalRecords={cards.length}
-                        onPageChange={onPageChange}
-                    />
-
+  return (
+    <div className=" bg-[#050618] flex flex-col gap-4">
+      <div className="p-6 flex flex-col gap-4">
+        <div className="flex justify-between flex-wrap gap-8">
+          <div className=" text-[28px] md:text-[40px] font-semibold w-[300px] text-white">
+            Auction Room
+          </div>
+          <GradientBtn
+            placeholder="Create Rooms"
+            onClick={() => {
+              setshow(!show);
+            }}
+          />
         </div>
+        <div className="header flex justify-between items-center sm:pr-28 flex-wrap gap-4">
+          <div
+            className="bg-inherit w-52 text-center outline-none cursor-pointer rounded-xl relative"
+            style={{
+              backgroundColor: "rgba(7, 38, 67, 1)",
+              boxShadow: "0 2.93px 3.67px 1.47px rgba(121, 197, 239, 0.38)",
+            }}
+          >
+            <div
+              className="w-full flex justify-between items-center p-6 py-2 capitalize font-semibold"
+              onClick={() => setShowOptions(!showOptions)}
+            >
+              {selectOption}{" "}
+              <IoIosArrowDown
+                className={`mt-1 transition-transform duration-500  ${
+                  showOptions ? "rotate-180" : ""
+                }`}
+              />
+            </div>
+            <div
+              className={`optionsConatiner top-14 absolute w-full left-0 rounded-xl z-50 overflow-hidden ${
+                showOptions ? "block" : "hidden"
+              }`}
+              style={{
+                backgroundColor: "rgba(7, 38, 67, 1)",
+                boxShadow: "0 2.93px 3.67px 1.47px rgba(121, 197, 239, 0.38)",
+              }}
+            >
+              {roomOptions.map((elem) => {
+                return (
+                  <div
+                    className="px-4 py-2 hover:bg-blue-950 active:text-sm h-10"
+                    key={elem}
+                    onClick={() => {
+                      setSelectOption(elem);
+                      setShowOptions(!showOptions);
+                    }}
+                  >
+                    {elem}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div
+            className="right flex items-center gap-2 cursor-pointer hover:bg-blue-950 rounded-xl px-4 py-2"
+            onClick={() => refresh(selectOption)}
+          >
+            Refresh
+            <RiRefreshLine />
+          </div>
+        </div>
+        <div className=" flex flex-col justify-center items-center w-[100%]">
+          <div className="flex flex-wrap gap-2 justify-evenly transition-all duration-200">
+            {/* {cards.slice(first, first + rows).map((card) => card)} */}
+            {selectOption == "free"
+              ? roomData.freeRooms.map((elem, index) => (
+                  <Card roomId={elem.RoomID} key={elem.RoomID + index} />
+                ))
+              : "premium"
+              ? roomData.premiumRooms.map((elem, index) => (
+                  <Card roomId={elem.RoomID} key={elem.RoomID + index} />
+                ))
+              : roomData.history.map((elem, index) => (
+                  <Card roomId={elem.RoomID} key={elem.RoomID + index} />
+                ))}
+          </div>
+        </div>
+      </div>
+      <DotPaginator
+        className="bg-[#050618] custom-paginator"
+        first={first}
+        rows={rows}
+        totalRecords={5}
+        onPageChange={onPageChange}
+      />
+    </div>
+  );
+};
 
-    )
-}
-
-export default Auctionrooms
+export default Auctionrooms;
--- client/src/Components/AuctionRoom/Card.jsx
+++ client/src/Components/AuctionRoom/Card.jsx
@@ -1,43 +1,52 @@
-import React, { useState } from 'react'
-import logo from '../../assets/AuctionroomImages/WonderKids Landing Page Exploration 1 (1).png'
-import eye from '../../assets/AuctionroomImages/mdi_eye.png'
-import time from '../../assets/AuctionroomImages/ion_time.png'
-import './Auctionroom.css'
-import GradientBtn from '../Buttons/GradientBtn'
+import React, { useState } from "react";
+import logo from "../../assets/AuctionroomImages/WonderKids Landing Page Exploration 1 (1).png";
+import eye from "../../assets/AuctionroomImages/mdi_eye.png";
+import time from "../../assets/AuctionroomImages/ion_time.png";
+import "./Auctionroom.css";
+import GradientBtn from "../Buttons/GradientBtn";
+import { useNavigate } from "react-router-dom";
 
-const Card = () => {
-  const [live,setlive] = useState(true) ;
+const Card = ({ roomId }) => {
+  const navigate = useNavigate();
+  const [live, setlive] = useState(true);
 
   return (
-    <div className='border relative w-[350px] rounded-xl bg-[#bec0dd1f] border-[#223534] text-white p-2 flex flex-col gap-2 justify-center items-center hover:scale-[1.02] transition ease-in-out hover:shadow-lg shadow-white ' >
-      {
-        live && 
+    <div className="border relative w-[350px] rounded-xl bg-[#bec0dd1f] border-[#223534] text-white p-2 flex flex-col gap-2 justify-center items-center hover:scale-[1.02] transition ease-in-out hover:shadow-lg shadow-white ">
+      {live && (
         <span className="absolute right-2 top-2 flex h-3 w-3">
-        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-      </span>
-      }
-      
-      <div className=' text-left w-full text-[16px] font-bold' >Wonder Kids Website</div>
-      <div>
-        <img className='rounded-lg' alt='' src={logo} />
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+        </span>
+      )}
+      <div className=" text-left w-full text-[16px] font-bold">
+        Wonder Kids Website
       </div>
-      <div className='flex w-full justify-between' >
-        <div className='flex gap-2 '>
-          <img alt='' src={eye} />
-          <span className=''>2:00pm</span>
+      <div>
+        <img className="rounded-lg" alt="" src={logo} />
+      </div>
+      <div className="flex w-full justify-between">
+        <div className="flex gap-2 ">
+          <img alt="" src={eye} />
+          <span className="">2:00pm</span>
         </div>
-        <div className='flex gap-2' >
-          <img alt='' src={time} />
+        <div className="flex gap-2">
+          <img alt="" src={time} />
           <span>45</span>
         </div>
       </div>
-      <div className='w-full hover:scale-[0.95] transition ease-in-out' >
-        <button className='text-[#11111] bg-gradient-to-b from-[#18203E] to-[#172748]rounded-full px-8 py-3 text-[18px] font-semibold rounded-full border w-full' >Enter</button>
+      <div className="w-full hover:scale-[0.95] transition ease-in-out">
+        <button
+          className="text-[#11111] bg-gradient-to-b from-[#18203E] to-[#172748]rounded-full px-8 py-3 text-[18px] font-semibold rounded-full border w-full"
+          onClick={() => {
+            console.log(roomId)
+            navigate(`/room/${roomId}`);
+          }}
+        >
+          Enter
+        </button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
