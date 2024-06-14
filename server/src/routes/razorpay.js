00be5fd4bbe20719cf052c5c44f69459cae6bf3c
JiteshVarade <jiteshvarade1@gmail.com>
2024-06-14 15:30:40 +0530

    Deposit Withdraw done

--- server/src/routes/razorpay.js
+++ server/src/routes/razorpay.js
@@ -9,6 +9,7 @@ const crypto = require('crypto')
 const {RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY, Webhook_Secret} = require('../../constants')
 const Payment = require('../models/payment')
 const User = require("../models/user")
+const sendEmail = require("../utils/email")
 
 const router = express.Router()
 
@@ -68,6 +69,54 @@ router.post("/verify",async (req,res)=>{
     {
         console.log("Payment unsuccessful")
         res.status(500)
+    }
+})
+
+router.post("/withdraw",async (req,res)=>{
+    const email = req.body.email
+    const amount = req.body.amount
+
+    try{
+        const user = await User.findOneAndUpdate({"UserInfo.email" : email},{
+            $push : {"Profile.Transactions" : {
+                email : email,
+                amount : amount,
+                category : "credit"
+            }},
+            $inc : {"Profile.Credits" : -amount}
+        })
+
+        const subject = "Confirmation of Your Withdrawal Request"
+
+        const html = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                <p>Dear ${user.UserInfo.name},</p>
+                
+                <p>I hope this email finds you well.</p>
+                
+                We are pleased to inform you that your withdrawal request of ₹${amount/100} has been successfully received and is currently being processed. You can expect the completion of the withdrawal process within the next 24 hours.</p>
+                
+                <p>Should you have any questions or need further assistance, please do not hesitate to reach out to our support team. We are here to help and ensure a smooth experience for you.</p>
+                
+                <p>Thank you for your patience and understanding.</p>
+                
+                <p>Best regards,</p><br>
+                <p>DevAuction</p>
+            </body>
+            </html>        
+        `
+
+        sendEmail(email,subject,html)
+
+        res.send("Wihtdraw successfull")
+    }catch(error){
+        console.log(error)
     }
 })
 
