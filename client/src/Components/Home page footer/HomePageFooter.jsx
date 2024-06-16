f56f05e6ae68b9ee67eef97a1997323b419b832e
Jitesh Varade <143549511+jiteshvarade@users.noreply.github.com>
2024-06-16 09:17:52 +0530

    Update HomePageFooter.jsx

--- client/src/Components/Home page footer/HomePageFooter.jsx
+++ client/src/Components/Home page footer/HomePageFooter.jsx
@@ -10,7 +10,7 @@ export default function HomePageFooter() {
     <>
       <section className='text-white py-10'>
         <div>
-          <div className='flex justify-between items-center flex-wrap gap-4 w-full px-10'>
+{/*           <div className='flex justify-between items-center flex-wrap gap-4 w-full px-10'>
 
           <h4 className='text-2xl text-center font-bold'><span className='text-4xl text-[#8ed7f8]'>DEV</span>AUCTION</h4>
           <ul className='flex text-base lg:text-lg flex-wrap  items-center text-white font-thin gap-3'>
@@ -20,15 +20,15 @@ export default function HomePageFooter() {
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>About Us</span></li>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>Contact Us</span></li>
           </ul>
-          </div>
+          </div> */}
 
           <hr className='mt-6 ' />
           <div className='flex justify-between text-xs lg:text-sm items-center px-10 mt-6'>
             <div>&copy; 2024, DevAuction All Rights Reserved</div>
             <div className='flex justify-between items-center gap-3 '>
-              <a href="#" target='_blank'><img width={40} src={youtubeIcon} alt="" /></a>
-              <a href="#" target='_blank'><img width={30} src={facebookIcon} alt="" /></a>
-              <a href="#" target='_blank'><img width={40} src={instagramIcon} alt="" /></a>
+              <a href="https://youtube.com/channel/UCTbpFySQiGvwgr_eAEINzhw?si=eYGThi3F-jXeaxWn" target='_blank'><img width={40} src={youtubeIcon} alt="" /></a>
+              <a href="https://www.facebook.com/jitesh.varade?mibextid=ZbWKwL" target='_blank'><img width={30} src={facebookIcon} alt="" /></a>
+              <a href="https://www.instagram.com/devauction?igsh=MWsyZjhpbm1zNjEwdw==" target='_blank'><img width={40} src={instagramIcon} alt="" /></a>
             </div>
           </div>
         </div>
