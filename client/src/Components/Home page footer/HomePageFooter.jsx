0281bdeaa62549068cc0dc8a2e3537416b240257
Jitesh Varade <143549511+jiteshvarade@users.noreply.github.com>
2024-06-16 11:20:06 +0530

    Update HomePageFooter.jsx

--- client/src/Components/Home page footer/HomePageFooter.jsx
+++ client/src/Components/Home page footer/HomePageFooter.jsx
@@ -10,17 +10,17 @@ export default function HomePageFooter() {
     <>
       <section className='text-white py-10'>
         <div>
-{/*           <div className='flex justify-between items-center flex-wrap gap-4 w-full px-10'>
+          <div className='flex justify-between items-center flex-wrap gap-4 w-full px-10'>
 
           <h4 className='text-2xl text-center font-bold'><span className='text-4xl text-[#8ed7f8]'>DEV</span>AUCTION</h4>
-          <ul className='flex text-base lg:text-lg flex-wrap  items-center text-white font-thin gap-3'>
+{/*           <ul className='flex text-base lg:text-lg flex-wrap  items-center text-white font-thin gap-3'>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>Home</span></li>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>How it Works</span></li>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>Features</span></li>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>About Us</span></li>
             <li><span className='hover:underline hover:text-[#8ed7f8] cursor-pointer'>Contact Us</span></li>
-          </ul>
-          </div> */}
+          </ul> */}
+          </div>
 
           <hr className='mt-6 ' />
           <div className='flex justify-between text-xs lg:text-sm items-center px-10 mt-6'>
